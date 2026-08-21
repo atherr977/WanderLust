f56a662570ef90b87a907b3a56cc266e696506f2
Athar Ashraf War <atherr977@gmail.com>
2026-08-22 00:43:43 +0530

    Getting started

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:"https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc",
+    set: (v) =>
+      v === ""
+        ? "https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
