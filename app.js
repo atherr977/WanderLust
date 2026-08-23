4094d23bbe21f3208a2059d38ab483d2acf07c36
Athar Ashraf War <atherr977@gmail.com>
2026-08-23 20:13:57 +0530

    Craete (New and Create Route)

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
-
+//ROOT Route
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
@@ -32,11 +32,24 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
+//New Route(Keeos this route above the show route
+// because otherwise it will be treated as an id and will not work)
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
 //Show Route
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
 
 
