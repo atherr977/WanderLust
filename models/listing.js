5cd36d0bf4e304947146ae4c2d756886b926e56a
Athar Ashraf War <atherr977@gmail.com>
2026-09-02 21:20:54 +0530

    Styling Show Listing

--- models/listing.js
+++ models/listing.js
@@ -9,10 +9,10 @@ const listingSchema = new Schema({
   description: String,
   image: {
    type: String,
-    default:"https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc",
+    default:"https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
-        ? "https://unsplash.com/photos/person-in-swimming-pool-during-daytime-rlwE8f8anOc"
+        ? "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
   price: Number,
