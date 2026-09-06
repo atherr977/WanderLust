c201ff00b7c27bd5c995be0cf9182051ff306834
Athar Ashraf War <atherr977@gmail.com>
2026-09-06 12:59:14 +0530

    Created a dedicated page for showing Erros(error.ejs)

--- app.js
+++ app.js
@@ -113,7 +113,8 @@ next(new ExpressError(404, "Page Not Found!"));
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
